c79e880f06a6c9af680b0d78fa40c130be84294f
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-08-28 09:43:31 +0530

    initial files

---
+++ IISHost/NorthwindClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NorthwindClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductServiceRef.ProductsClient client = new ProductServiceRef.ProductsClient();
+            string category = client.GetCategoryName(1);
+            string name = client.GetProductName(1);
+            int qty = client.GetProductQty(1);
+            Console.WriteLine("Product Name : " + name);
+            Console.WriteLine("Product Qty : " + qty.ToString());
+            Console.WriteLine("Product Category : " + category);
+            Console.Read();
+        }
+    }
+}
